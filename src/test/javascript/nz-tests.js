a45a96c429bf1e13192d1eb6f5cf49d28e575379
Mike Beaulieu <MikeBeaulieu@users.noreply.github.com>
2017-06-02 20:01:33 -0600

    Initial commit

---
+++ src/test/javascript/nz-tests.js
@@ -0,0 +1,23 @@
+var Jsonix = require('jsonix').Jsonix;
+var NZ = require('../../../target/generated-sources/xjc/NewZealand.std').NZ;
+
+module.exports = {
+	"UnmarshalFile": function(test) {
+
+		// Create Jsonix context
+		var context = new Jsonix.Context([ NZ ]);
+		
+		// Create unmarshaller
+		var unmarshaller = context.createUnmarshaller();
+		
+		// Unmarshal the XML file
+		unmarshaller.unmarshalFile( 'src/test/resources/nz-sample-xml.xml',
+			function(reportElement) {
+				
+				var report = reportElement.value;
+
+				test.equal('10109', report.rentityId);
+				test.done();
+		});
+        }
+};
